4c17c388ef09c19f60ea55c35a558329ef436d90
Daman431 <damansharma8k@gmail.com>
2023-11-17 19:52:14 +0530

    Updates meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{background: 'url(https://i.pinimg.com/originals/03/f1/dd/03f1dd0ac70cbb94211577d3dc854ccd.jpg)',backgroundPosition: 'top',backgroundRepeat: 'no-repeat'}}>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2" style={{background: 'url(/stars-bg.jpg)',backgroundPosition: 'top',backgroundRepeat: 'no-repeat'}}>
         <h1 className="mt-2 text-5xl font-bold">Coming Soon</h1>
         <p className="mt-4 text-2xl font-bold">Patience is a virtue. A portfolio in progress - watch this space for the big reveal!</p>
       </div>
